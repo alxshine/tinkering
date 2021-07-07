0acb2ecde1bd557811e988e55390e5d95d9e7e11
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2021-07-07 18:16:51 +0200

    Extend powerboard MC to memory and cpu

--- powerboard/powerboard-peripheral/src/main.cpp
+++ powerboard/powerboard-peripheral/src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 #include "pins.h"
 
-const int CLOCK = D0;
-const int SHIFT = D1;
-const int CLEAR = D2;
+const int CLOCK = D7;
+const int CPU_SHIFT = D1;
+const int MEM_SHIFT = D2;
+const int CLEAR = D6;
 const int DELAY_TIME = 0;
 const int UPDATE_DELAY = 100;
 
@@ -13,20 +14,21 @@ void clear()
   digitalWrite(CLEAR, HIGH);
 }
 
-void shiftBit(bool value)
+void shiftBits(bool cpu_value, bool mem_value)
 {
-  digitalWrite(SHIFT, value);
+  digitalWrite(CPU_SHIFT, cpu_value);
+  digitalWrite(MEM_SHIFT, mem_value);
   digitalWrite(CLOCK, HIGH);
   delay(DELAY_TIME);
   digitalWrite(CLOCK, LOW);
 }
 
-void showBarValue(unsigned char value)
+void showBarValues(unsigned char cpu_value, unsigned char mem_value)
 {
   clear();
   for (char i = 8; i > 0; --i)
   {
-    shiftBit(value >= i);
+    shiftBits(cpu_value >= i, mem_value >= i);
   }
 }
 
@@ -35,8 +37,11 @@ void setup()
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(CLOCK, OUTPUT);
-  pinMode(SHIFT, OUTPUT);
+
   pinMode(CLEAR, OUTPUT);
+
+  pinMode(CPU_SHIFT, OUTPUT);
+  pinMode(MEM_SHIFT, OUTPUT);
 
   digitalWrite(CLEAR, HIGH);
 
@@ -48,8 +53,11 @@ void loop()
   while (!Serial.available())
     delay(100);
   auto read = Serial.readString();
-  auto value = read.toInt();
+  int cpu, mem;
+  sscanf(read.c_str(), "%d,%d", &cpu, &mem);
 
-  showBarValue(value);
-  Serial.println(value);
+  showBarValues(cpu, mem);
+  Serial.print(cpu);
+  Serial.print(",");
+  Serial.println(mem);
 }
